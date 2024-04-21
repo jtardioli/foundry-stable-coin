e5dd695ccbf38122aecd9fc91e8ac43249e8681d
jtardioli <joshua.tardioli@gmail.com>
2024-04-21 19:36:12 -0300

    finish protoocl invariant tests

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -303,4 +303,12 @@ contract DSCEngine is ReentrancyGuard {
     {
         return _getAccountInformation(user);
     }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
+
+    function getCollateralBalanceOfUser(address user, address token) external view returns (uint256) {
+        return s_collateralDeposited[user][token];
+    }
 }
